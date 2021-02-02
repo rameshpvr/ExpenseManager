0576cf62c0102b0c84d8758b764cad7fd58a9b86
Ramesh <rameshpvr97@gmail.com>
2021-02-02 19:59:50 +0530

    Updates

---
+++ precache-manifest.4de8fe94c2cc42d5779c2d1f295e0969.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686a4ddf92d2cc90cb18ba97b214ac45",
+    "url": "/ExpenseManager/index.html"
+  },
+  {
+    "revision": "d71b098fa290fdd81edb",
+    "url": "/ExpenseManager/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "0de049b4a5ca768a9355",
+    "url": "/ExpenseManager/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "d71b098fa290fdd81edb",
+    "url": "/ExpenseManager/static/js/2.f5646b36.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/ExpenseManager/static/js/2.f5646b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de049b4a5ca768a9355",
+    "url": "/ExpenseManager/static/js/main.a1f874dc.chunk.js"
+  },
+  {
+    "revision": "b6176aa9ccb936f01fcd",
+    "url": "/ExpenseManager/static/js/runtime-main.0f4331ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ExpenseManager/static/media/logo.5d5d9eef.svg"
+  }
+]);
